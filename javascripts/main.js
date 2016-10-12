5ed791318863b2407d8c89fdeeb8038361f19462
Wojciech Pospieszynski <pospieszynski.wojciech@gmail.com>
2016-10-12 14:12:48 +0200

    encapsulate lake area in the component

---
+++ javascripts/main.js
@@ -0,0 +1,332 @@
+require("../stylesheets/screen.css");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Lake extends React.Component {
+    render() {
+        return (
+            <table id="lake">
+                <thead>
+                <tr>
+                    <th colSpan={10}>Lake</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>
+                        <label className="frog male">
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label className="frog female">
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                    <td>
+                        <label>
+                            <input type="checkbox" />
+                        </label>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        )
+    }
+}
+ReactDOM.render(<Lake/>, document.getElementById('lake-area'));
